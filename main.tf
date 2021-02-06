2af947a9576f79d76dccfdb6c9854880b2c631e1
Oleksii Shkurupii <Oleksii.Shkurupii@playtech.com>
2021-02-06 19:37:13 +0200

    Added ACM

--- main.tf
+++ main.tf
@@ -9,3 +9,47 @@ variable "bucket_name" {
 resource "aws_s3_bucket" "test-bucket" {
   bucket = var.bucket_name
 }
+
+variable "domain_name" {
+  type = string
+  description = "Domain Name"
+}
+
+variable "alternative_names" {
+  type = list(string)
+  description = "Alternative Domain Names"
+}
+
+resource "aws_acm_certificate" "cert" {
+  domain_name               = var.bucket_name
+  subject_alternative_names = var.alternative_names
+  validation_method         = "DNS"
+}
+
+data "aws_route53_zone" "primary" {
+  name         = var.domain_name
+  private_zone = false
+}
+
+resource "aws_route53_record" "my_record" {
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name    = dvo.resource_record_name
+      record  = dvo.resource_record_value
+      type    = dvo.resource_record_type
+      zone_id = data.aws_route53_zone.primary.zone_id
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = each.value.zone_id
+}
+
+resource "aws_acm_certificate_validation" "validation" {
+  certificate_arn         = aws_acm_certificate.cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.my_record : record.fqdn]
+}
