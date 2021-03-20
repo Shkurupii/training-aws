7051bf82216a9f832e2a640806e9e96362e08aa2
Oleksii Shkurupii <Oleksii.Shkurupii@playtech.com>
2021-03-20 15:27:27 +0200

    Refactored code

--- iam-users-audit.tf
+++ iam-users-audit.tf
@@ -1,4 +1,4 @@
-module "iam-credentials" {
+module "audit-iam-credentials" {
   source = "./modules/iam-credentials"
   account_id = var.audit_account_id
   account_user = var.audit_account_user
--- iam-users-develop.tf
+++ iam-users-develop.tf
@@ -1,4 +1,4 @@
-module "iam-credentials" {
+module "develop-iam-credentials" {
   source = "./modules/iam-credentials"
   account_id = var.develop_account_id
   account_user = var.develop_account_user
